4c3aebe9e683d9977923915e02c1445e3633b49d
Muhammad Irfan <muhammadirfanamin075@gmail.com>
2025-12-03 11:00:22 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,55 @@
+// Q#1
+// var arr = [20, 30, 40, 50, 60];
+// var Largest = arr[0];
+// for (var i = 1; i < arr.length; i++){
+//     if(arr[i] > Largest){
+//         Largest = arr[i]
+//     }
+// }
+// console.log("The given number is: " + arr)
+// console.log("The largest number is: " + Largest);
+
+
+// Q#2
+// console.log('Given Array:')
+// var arr = [1,2,3,4,5,6,7,8,9,10];
+// console.log(arr)
+// console.log('Even numbers from the given array are: ')
+// for (var i = 1; i <= arr.length; i++){
+//     if(i%2==0){
+//         console.log(i)
+//     }
+// }
+
+
+// Q#3
+// var arr = [1,2,3,4,5,6,7,8,9,10]
+// console.log('Original Array: ' + arr)
+// console.log("Reverse Counting:")
+// for(var i=10; i>=1; i--){
+//     console.log(i)
+// };
+
+
+//Q#4
+// var arr1 = [1 , 2 , 3];
+// var arr2 = [3 , 4 , 5];
+// for (var i=0; i<arr1.length; i++){
+//     if(arr2.indexOf(arr1[i]) == -1) {
+//        arr2.push(arr1[i]);
+//     }
+// }
+// console.log('A U B: ', arr2.sort());
+
+
+// Q#5
+// var arr=[1,2,3,4,5];
+// console.log("Given numbers are: "+arr)
+// var total=0;
+// for(var i=0;i<arr.length;i++){
+//     total+=arr[i]
+// }
+// console.log("Total: " + total)
+
+
+// Q#6
